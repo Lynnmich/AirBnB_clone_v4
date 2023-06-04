327c0afe4dbf264a920bcabee8fb6fb5fc318cf4
Lynne Michuki <lynnenmichuki@gmail.com>
2023-06-04 15:10:56 +0300

    Task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  const amenities = {};
+  $('input[type=checkbox]').change(function() {
+  const amenityId = $(this).data('amenity-id');
+
+  if ($(this).is('checked')) {
+    amenityies[amenityId] = $(this).data('amenity-name');
+  } else {
+    delete amenities[amenityId];
+  }
+
+  const amenitiesList = Object.values(amenities).join(', ');
+  $('div.Amenities > h4').text(amenitiesList);
+  });
+});	
